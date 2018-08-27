dac32907724880f9dbcb1dbbe150c49fcebc39ac
maromato <maromato2012@gmail.com>
2018-08-27 19:30:26 -0400

    Create SliceStack_MF_2.py

---
+++ SliceStack_MF_2.py
@@ -0,0 +1,45 @@
+import os
+import re
+import time
+import pickle
+from ij.gui import GenericDialog
+from ij.io import DirectoryChooser, FileSaver,OpenDialog
+from ij import IJ, ImagePlus
+from java.lang import Runtime, Runnable 
+from loci.plugins.util import BFVirtualStack
+from loci.formats import ChannelSeparator
+ 
+def run():
+  srcDir = DirectoryChooser("Choose directory").getDirectory()
+  if not srcDir:
+    return
+  
+  targetDir = DirectoryChooser("Choose target directory").getDirectory()
+  if targetDir is None:
+    # User canceled the dialog
+    return
+  
+  sId = ".tiff"  	
+
+  
+  for root, directories, filenames in os.walk(srcDir):
+    for filename in filenames:
+      path = os.path.join(root,filename)
+      if not (sId in filename):
+         continue
+ 
+      cs = ChannelSeparator()
+      cs.setId(path)
+      print "cs" ,cs
+      iStack=1
+      iSample=-(-iStack//4)
+      bf = BFVirtualStack(path, cs, False, False, False)
+      for sliceIndex in xrange(1, bf.getSize() +1):
+        print "Processing slice", sliceIndex
+        ip = bf.getProcessor(sliceIndex)
+        sliceFileName = os.path.join(targetDir, str(iSample)", filename+"_"+str(sliceIndex) + ".tiff")
+        print "writing ", sliceFileName
+        #FileSaver(ImagePlus(str(sliceIndex), ip)).saveAsTiff(sliceFileName)
+      iStack +=1
+ 
+run()
