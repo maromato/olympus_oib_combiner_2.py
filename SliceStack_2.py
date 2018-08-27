daa871b8b8297b7544f743847943458fa7b4e1d7
maromato <maromato2012@gmail.com>
2018-08-27 18:28:35 -0400

    Update SliceStack_2.py

--- SliceStack_2.py
+++ SliceStack_2.py
@@ -35,7 +35,7 @@ srcDir = DirectoryChooser("Choose directory").getDirectory()
       for sliceIndex in xrange(1, bf.getSize() +1):
         print "Processing slice", sliceIndex
         ip = bf.getProcessor(sliceIndex)
-        sliceFileName = os.path.join(targetDir+str(sliceIndex) + ".tif")
+        sliceFileName = os.path.join(targetDir+filename+"_"+str(sliceIndex) + ".tif")
         print sliceFileName
         #FileSaver(ImagePlus(str(sliceIndex), ip)).saveAsTiff(sliceFileName)
  
