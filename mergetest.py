b4395de3104679f1ee043906612cadf09a1c332f
maromato <maromato2012@gmail.com>
2018-08-28 16:04:39 -0400

    Update mergetest.py

--- mergetest.py
+++ mergetest.py
@@ -5,6 +5,7 @@ import pickle
 from ij.gui import GenericDialog
 from ij.io import DirectoryChooser, FileSaver
 from ij import IJ, ImagePlus
+from ij.plugin import ChannelSplitter, StackCombiner
 from java.lang import Runtime, Runnable
 
 
@@ -22,6 +23,7 @@ def loadStacks():
   sId = ".tiff"
   
   iStack = 0
+ 
   for root, directories, filenames in os.walk(srcDir):
     for filename in filenames:
       path = os.path.join(root,filename)
@@ -30,15 +32,20 @@ def loadStacks():
 
       IJ.log("opening "+filename)
       imp = IJ.openImage(path)
+      #c[iStack]=imp
       imp.show()
       imp.setTitle("st"+str(iStack))  # in order of the combining to work additive
       print filename, str(iStack)
-      
-    imp2=IJ.run("Merge Channels...", "c1=st1 c2=st0 c3=st3 c4=st2");
+      iStack +=1
+
+    #imp2 = StackCombiner().combineHorizontally(c[0],c[1])
+    imp2=IJ.run("Merge Channels...", "c1=st1 c2=st0 c3=st3 c4=st2  create composite keep");
+    print 'ok'
     fs=FileSaver(imp2)
-    fs.saveAsTiff(srcDir+"merge_"+filename)
-    IJ.log("convert back to normal stack: Image...Hyperstacks...Hyperstack to Stack")
-    IJ.log("if you got an error message from the hyperstack you probably ran out of memory and it did not load all files!")
+    print 'OK2'
+    saveAsTiff(srcDir+"merge_"+filename)
+    #IJ.log("convert back to normal stack: Image...Hyperstacks...Hyperstack to Stack")
+    #IJ.log("if you got an error message from the hyperstack you probably ran out of memory and it did not load all files!")
      
 def run():
   loadStacks()
