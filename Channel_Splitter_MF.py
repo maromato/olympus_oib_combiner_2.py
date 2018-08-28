15af308bf6fa0b1437d8bdabcad1f3c45339d5b0
maromato <maromato2012@gmail.com>
2018-08-28 17:15:43 -0400

    Update Channel_Splitter_MF.py

--- Channel_Splitter_MF.py
+++ Channel_Splitter_MF.py
@@ -1,64 +1,101 @@
 import os
-import re
-import time
-import pickle
-from ij.gui import GenericDialog
-from ij.io import DirectoryChooser, FileSaver, OpenDialog
-from ij import IJ, ImagePlus, ImageStack
-from java.lang import Runtime, Runnable
-from loci.plugins.util import BFVirtualStack
-from loci.formats import ChannelSeparator
 
-def extractChannel(imp, nChannel, nFrame):
- #""" Extract a stack for a specific color channel and time frame """
- stack = imp.getImageStack()
- ch = ImageStack(imp.width, imp.height)
- for i in range(1, imp.getNSlices() + 1):
-   index = imp.getStackIndex(nChannel, i, nFrame)
-   ch.addSlice(str(i), stack.getProcessor(index))
- return ImagePlus("Channel " + str(nChannel), ch)
+import re
+
+import time
+
+import pickle
+
+from ij.gui import GenericDialog
+
+from ij.io import DirectoryChooser, FileSaver
+
+from ij import IJ, ImagePlus
+
+from ij.plugin import ChannelSplitter, StackCombiner
+
+from java.lang import Runtime, Runnable
+
+
+
 
 
 def loadStacks():
+
  
+
+  
+
   IJ.log(" "); 
-  IJ.log("Channel_Splitter_ver1.0; 08/24/2018; maromato2012@gmail.com"); 
-  IJ.log("tested with: ImageJ2 with jython-shaded-2.5.3.jar, Fiji updated 8/24/2018"); 
+
+  IJ.log("oib concatenator v0.5.1; 2012; tischer@embl.de; almf@embl.de; revised by maromato2012@gmail.com"); 
+
+  IJ.log("tested with: ImageJ 1.47a; FIJI update on July 20th 2012"); 
+
    
+
   srcDir = DirectoryChooser("Choose directory").getDirectory()
+
   if not srcDir:
+
     return
-  
+
   targetDir = DirectoryChooser("Choose target directory").getDirectory()
+
   if targetDir is None:
+
     # User canceled the dialog
+
     return
+
+  sId = ".tiff"
+
   
-  sId = ".tiff"  	
+
   iStack = 0
-  
+
+ 
+
   for root, directories, filenames in os.walk(srcDir):
+
     for filename in filenames:
+
       path = os.path.join(root,filename)
+
       if not (sId in filename):
+
         continue
 
-      IJ.log("opening "+filename)
-      imp = IJ.openImage(path)
-      imp.show()
-      nChannels = imp.getNChannels()
-      imp = IJ.getImage()
-      for j in range(1,nChannels+1):
-       imp2=extractChannel(imp, j, 1)
-       imp2.show()
-       fs=FileSaver(imp2)
-       fs.saveAsTiff(os.path.join(targetDir+filename+"_c"+str(j)+".tiff"))
-        
-      iStack = iStack + 1
-      print(iStack)
-   
- 
-  IJ.log("if you got an error message from the hyperstack you probably ran out of memory and it did not load all files!")
 
-loadStacks()
- 
+
+      IJ.log("opening "+filename)
+
+      imp = IJ.openImage(path)
+
+      
+
+      imp.show()
+
+      imp.setTitle("st"+str(iStack))  # in order of the combining to work additive
+
+      print filename, str(iStack)
+
+      iStack +=1
+
+
+
+    
+
+    imp2=IJ.run("Merge Channels...", "c1=st1 c2=st0 c3=st3 c4=st2  create composite");
+
+    IJ.save(imp2,os.path.join(targetDir,filename+"_merge"))
+
+         
+
+def run():
+
+  loadStacks()
+
+    
+
+run()
